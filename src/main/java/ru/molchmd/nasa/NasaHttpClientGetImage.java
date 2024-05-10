20bd9ec0506328e7cef91473b48a62622446527e
Maria Molchanova <molchmd@gmail.com>
2024-05-10 19:51:14 +0300

    Добавлен метод getPhoto

--- src/main/java/ru/molchmd/nasa/NasaHttpClientGetImage.java
+++ src/main/java/ru/molchmd/nasa/NasaHttpClientGetImage.java
@@ -5,29 +5,47 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import ru.molchmd.Settings;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class NasaHttpClientGetImage {
     private final CloseableHttpClient httpclient;
     private final ObjectMapper mapper;
-    private HttpGet getRequest;
+    private final HttpGet getRequest;
     private CloseableHttpResponse getResponse;
     private NasaImageData image;
+
+    public String getNasaImageName() {
+        return image.getTitle();
+    }
+
+    private InputFile nasaPhoto;
     private final String GENERAL_API_NASA_URL = "https://api.nasa.gov/planetary/apod?thumbs=True&api_key=";
     public final String ERROR_URL =
             "https://www.funnyart.club/uploads/posts/2022-12/1671990510_www-funnyart-club-p-memi-s-kotom-vkontakte-17.jpg";
+    public final InputFile ERROR_PHOTO;
 
     public NasaHttpClientGetImage() {
         httpclient = HttpClients.createDefault();
         mapper = new ObjectMapper();
+        getRequest = new HttpGet();
 
-        getRequest = new HttpGet(GENERAL_API_NASA_URL + Settings.NASA_API_KEY);
+        File error_photo = new File("src/main/resources/error.jpg");
+        ERROR_PHOTO = new InputFile(error_photo);
     }
 
     public String getImageURL() {
         try {
+            try {
+                getRequest.setURI(new URI(GENERAL_API_NASA_URL + Settings.NASA_API_KEY));
+            }
+            catch (URISyntaxException e) {
+                System.out.println("! Error: error setURI");
+            }
             getResponse = httpclient.execute(getRequest);
 
             try {
@@ -39,8 +57,28 @@ public class NasaHttpClientGetImage {
             }
         }
         catch (IOException e) {
-            System.out.println("Error: NasaHttpClientGetImage");
+            System.out.println("! Error: return a ERROR_URL");
             return ERROR_URL;
         }
+    }
+    public InputFile getPhoto() {
+        try {
+            getRequest.setURI(new URI(getImageURL()));
+        }
+        catch (URISyntaxException e) {
+            System.out.println("! Error: error setURI");
+        }
+
+        try {
+            getResponse = httpclient.execute(getRequest);
+            InputStream inStream = getResponse.getEntity().getContent();
+            nasaPhoto = new InputFile(inStream, "photo");
+            return nasaPhoto;
+        }
+        catch (IOException e) {
+            System.out.println("! Error: return ERROR_PHOTO");
+            return ERROR_PHOTO;
+        }
+
     }
 }
